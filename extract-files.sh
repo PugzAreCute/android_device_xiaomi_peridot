cbfd8dc6959269d62ef79e66d96cd3e19eaeb553
Arian <arian.kulmer@web.de>
2024-09-06 14:01:17 +0200

    peridot: Use libhidlbase_shim for vendor.libdpmframework
    Change-Id: Iefe50b82b5b13bbdee10819a0c60c14240fab7ce

--- extract-files.sh
+++ extract-files.sh
@@ -95,6 +95,10 @@ function blob_fixup() {
 	    [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --add-needed "libhidlbase_shim.so" "${2}"
             ;;
+        vendor/lib64/vendor.libdpmframework.so)
+	    [ "$2" = "" ] && return 0
+            "${PATCHELF}" --add-needed "libhidlbase_shim.so" "${2}"
+            ;;
         *)
             return 1
             ;;
