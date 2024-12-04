fa814e4fd5ea660c6930f5d260d624a0c3f707a0
ath1188 <111036482+athizz2005@users.noreply.github.com>
2024-12-04 14:13:52 +0000

    peridot: Bringup @ProjectInfinityX

--- infinity_peridot.mk
+++ infinity_peridot.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2024 The LineageOS Project
+# Copyright (C) 2024 The infinityOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common infinity stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Inherit from peridot device
 $(call inherit-product, device/xiaomi/peridot/device.mk)
@@ -17,7 +17,7 @@ $(call inherit-product, device/xiaomi/peridot/device.mk)
 # Inherit from the MiuiCamera setup
 $(call inherit-product-if-exists, device/xiaomi/peridot-miuicamera/device.mk)
 
-PRODUCT_NAME := lineage_peridot
+PRODUCT_NAME := infinity_peridot
 PRODUCT_DEVICE := peridot
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
@@ -27,23 +27,18 @@ PRODUCT_SYSTEM_NAME := peridot_global
 PRODUCT_SYSTEM_DEVICE := peridot
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# RisingTechOSS Flags
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_DEFAULT_PIXEL_LAUNCHER := true
-TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
-
-# GMS
-WITH_GMS := true
-TARGET_CORE_GMS := false
-TARGET_CORE_GMS_EXTRAS := false
-
-# Maintainer
-RISING_MAINTAINER := AtharvaSwamy
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="peridot_global-user 14 UKQ1.240116.001 V816.0.10.0.UNPMIXM release-keys" \
-    DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
-    RisingChipset="8sGEN3" \
-    RisingMaintainer="AtharvaSwamy"
+    BuildFingerprint=POCO/peridot_global/peridot:14/UKQ1.240116.001/V816.0.10.0.UNPMIXM:user/release-keys \
+    DeviceName=peridot
+    DeviceProduct=peridot_global \
+    SystemName=peridot_global \
+    SystemDevice=peridot
 
-BUILD_FINGERPRINT := POCO/peridot_global/peridot:14/UKQ1.240116.001/V816.0.10.0.UNPMIXM:user/release-keys
+# Infinity-X Flags
+INFINITY_BUILD_TYPE := OFFICIAL
+INFINITY_MAINTAINER := AtharvaSwamy
+TARGET_SUPPORTS_BLUR := true
+TARGET_HAS_UDFPS := true
+WITH_GAPPS := true
